0cb5545beaaa68d61e309306cdab90b290b16d25
lile <674e.4e50@gmail.com>
2019-09-11 17:37:51 +0800

    1、修改数据库连接池 2、修改电信资费提示框大小

--- src/main/java/com/hrtxn/ringtone/project/threenets/threenet/service/ThreeNetsService.java
+++ src/main/java/com/hrtxn/ringtone/project/threenets/threenet/service/ThreeNetsService.java
@@ -250,7 +250,8 @@ public class ThreeNetsService {
      * @return
      */
     public static boolean isMobileNO(String mobiles) {
-        String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
+        //String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
+        String regex = "^(1[3-9])\\d{9}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(mobiles);
         return m.matches();
