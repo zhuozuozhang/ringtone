e66eebbfc6142edda938664db57bf68bd39ed7a4
lxp <578466286@qq.com>
2019-11-01 09:15:22 +0800

    权限问题

--- src/main/java/com/hrtxn/ringtone/project/telcertification/service/TelCertificationService.java
+++ src/main/java/com/hrtxn/ringtone/project/telcertification/service/TelCertificationService.java
@@ -82,6 +82,9 @@ public class TelCertificationService {
                 //对联系人电话的处理
 //                String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
 //                Pattern p = Pattern.compile(regex);
+                if(request.getUserId() == 16){
+                    request.setUserId(null);
+                }
                 //根据成员电话号查找
                 List<CertificationChildOrder> ccList = certificationChildOrderMapper.findTheChildOrder(page, request);
                 Integer[] ids = new Integer[100];
